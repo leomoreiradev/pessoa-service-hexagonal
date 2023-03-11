5f4899bd516939532dc1f57eaa04d0fd095cef4a
Leandro Moreira <leandrodev.oliveira@gmail.com>
2023-03-11 19:07:49 -0300

    Corrige swagger

--- src/main/java/com/leomoreiradev/pessoaservicehexagonal/config/SwaggerConfig.java
+++ src/main/java/com/leomoreiradev/pessoaservicehexagonal/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.leomoreiradev"))
+                .apis(RequestHandlerSelectors.basePackage("com.leomoreiradev.pessoaservicehexagonal"))
                 .build()
                 .apiInfo(ApiInfo.DEFAULT);
 
