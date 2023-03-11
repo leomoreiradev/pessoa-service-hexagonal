799287289de6226be81e227cad7969c6630fc9ff
Leandro Moreira <leandrodev.oliveira@gmail.com>
2023-03-11 17:03:02 -0300

    adiciona seed de banco de dados h2 com import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_pessoa (id, name) values (1,'Joe');
+INSERT INTO tb_pessoa (id, name) values (2,'Mary');
+INSERT INTO tb_pessoa (id, name) values (3,'Peter');
+INSERT INTO tb_pessoa (id, name) values (4,'Mike');
